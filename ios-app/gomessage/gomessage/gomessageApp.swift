7f60ea5d51a813c93120f2dd83534ea74566e758
Velda <ueldo343@gmail.com>
2025-08-24 21:44:03 +0300

    adding ios app(fully working)

---
+++ ios-app/gomessage/gomessage/gomessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  gomessageApp.swift
+//  gomessage
+//
+//  Created by Игорь on 24.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct gomessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
